55c21625fc7e08502f00d2e4f914cb2a31c4b2fe
IBIRASA <d.ishimwe@alustudent.com>
2023-07-26 06:37:08 -0700

    Size validation

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """
-This is a file of a class with
-private attribute
+This is a file of a class
+with an integer attribute
 """
 
 
 class Square:
     """
-    Class with private attribute size
+    This is a class with an integer attribute
     """
-    def __init__(self, size):
-        self.__size = size
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
