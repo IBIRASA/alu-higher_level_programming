927bbde0957eb62573515ab1055f15cdca171c26
IBIRASA <d.ishimwe@alustudent.com>
2023-08-18 00:42:20 -0700

    advanced tasks

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using Numpy"""
-import numpy
+"""
+This is the lazy_matrix_mul module.
+This module supplies one function, lazy_matrix_mul().
+"""
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Matrix multiplication using Numpy
-
-    :param m_a: The first matrix
-    :type m_a: list of list of ints/floats
-    :param m_b: The second matrix
-    :type m_b: list of list of int/floats
-    :returns: The results of the matrix multiplication
-    :rtype: list of list of int/floats
     """
-    
-    return(numpy.matmul(m_a, m_b))
+    just a function
+    using a module
+    """
+    return np.matmul(m_a, m_b)
