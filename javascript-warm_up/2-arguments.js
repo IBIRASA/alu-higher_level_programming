d6c2cd681de5688f8b6f272e8e53921d64975bc8
IBIRASA <d.ishimwe@alustudent.com>
2023-09-29 04:46:26 -0700

    arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,6 +1,11 @@
 #!/usr/bin/node
-if (process.argv[2]) {
-  console.log(process.argv[2]);
-} else {
+const args = process.argv.slice(2);
+const numArgs = args.length;
+
+if (numArgs === 0) {
   console.log('No argument');
+} else if (numArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
