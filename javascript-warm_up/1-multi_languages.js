3da5dc7dfe0b476457631b8b9faceca0612752cf
IBIRASA <d.ishimwe@alustudent.com>
2023-09-29 04:17:32 -0700

    3 languages

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const var1 = 'C is fun ';
-const var2 = 'Python is cool';
-const var3 = 'Javascript is amazing';
+const var1 = 'C is fun \n ';
+const var2 = 'Python is cool \n ';
+const var3 = 'Javascript is amazing \n';
 console.log(var1, var2, var3);
